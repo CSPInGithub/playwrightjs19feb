9adf3cba56f2422ce08b5d95d8dab5e041a8836c
shekhar <reach2shekhar@gmail.com>
2025-02-19 16:35:19 +0530

    updst ci

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 
-test.skip('first test', async ({ page }) => {
+test('first test', async ({ page }) => {
 
     await page.goto('https://www.saucedemo.com/')
     await page.locator('[placeholder="Username"]').fill('standard_user')
