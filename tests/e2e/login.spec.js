a2c8be6d19a5ea48567786c09e6c9c8931625165
shekhar <reach2shekhar@gmail.com>
2025-02-19 10:57:21 +0530

    assertionsadded

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -9,17 +9,26 @@ test('first test', async ({ page }) => {
 
     await page.locator("[id='login-button']").click()
 
-      //assertions
+    //assertions
 
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
 
     await expect(page.locator('button#add-to-cart-sauce-labs-backpack')).toBeEnabled()
 
+    // Soft assertion (does not stop execution if it fails)
+    try {
+        await expect.soft(page.locator('button#add-to-cart-sauce-labs-backpack')).toHaveText("Add to cart");
+    } catch (error) {
+        console.log("Soft assertion failed, but test continues...");
+    }
+
+
+
     await expect(page.locator('text=Products')).toBeVisible();
 
-  
 
-   
+
+
 
 
 
